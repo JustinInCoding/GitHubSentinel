6729648309bebade6b8bc1e247572fb24d3f3f57
JustinSheng <justin.wang4@icloud.com>
2025-06-14 22:20:12 +0800

    use deekseek instead

--- src/llm.py
+++ src/llm.py
@@ -5,7 +5,11 @@ from openai import OpenAI
 
 class LLM:
     def __init__(self):
-        self.client = OpenAI()
+        api_key = os.getenv("DEEPSEEK_API_KEY")
+        self.client = OpenAI(
+            api_key=api_key,
+            base_url="https://api.deepseek.com/v1",  # DeepSeek API 的基地址
+        )
 
     def generate_daily_report(self, markdown_content, dry_run=False):
         prompt = f"以下是项目的最新进展，根据功能合并同类项，形成一份简报，至少包含：1）新增功能；2）主要改进；3）修复问题；:\n\n{markdown_content}"
@@ -14,13 +18,13 @@ class LLM:
                 f.write(prompt)
             return "DRY RUN"
 
-        print("Before call GPT")
+        print("Before call deepseek")
         response = self.client.chat.completions.create(
-            model="gpt-3.5-turbo",
+            model="deepseek-chat",
             messages=[
                 {"role": "user", "content": prompt}
             ]
         )
-        print("After call GPT")
+        print("After call deepseek")
         print(response)
         return response.choices[0].message.content
