e04ddf7d8365139e916501a48d5c9f53c51736ed
JustinSheng <justin.wang4@icloud.com>
2025-06-14 22:29:36 +0800

    feat: update prompt add system role

--- src/llm.py
+++ src/llm.py
@@ -12,7 +12,7 @@ class LLM:
         )
 
     def generate_daily_report(self, markdown_content, dry_run=False):
-        prompt = f"以下是项目的最新进展，根据功能合并同类项，形成一份简报，至少包含：1）新增功能；2）主要改进；3）修复问题；:\n\n{markdown_content}"
+        prompt = f"{markdown_content}"
         if dry_run:
             with open("daily_progress/prompt.txt", "w+") as f:
                 f.write(prompt)
@@ -22,6 +22,7 @@ class LLM:
         response = self.client.chat.completions.create(
             model="deepseek-chat",
             messages=[
+                {"role": "system", "content": "你需要将用户输入项目的最新进展，根据功能合并同类项，形成一份简报，至少包含：1）新增功能；2）主要改进；3）修复问题；请注意，简报需要简洁明了，便于阅读和理解。"},
                 {"role": "user", "content": prompt}
             ]
         )
